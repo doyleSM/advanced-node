59e32c7b497738ffd8551d7baac043e392a39e2a
Gabriel Doyle <gdoyle.dev@gmail.com>
2021-07-18 21:26:35 -0300

    ensure service return AuthenticationError when loadFacebookUserApi return undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features/facebook-authentication'
 
 class FacebookAuthenticationService {
@@ -5,24 +6,28 @@ class FacebookAuthenticationService {
 
   }
 
-  async perform (params: FacebookAuthentication.Params): Promise<void> {
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUserApi.loadUserByToken(params)
+    return new AuthenticationError()
   }
 }
 
 interface LoadFacebookUserApi {
-  loadUserByToken: (params: LoadFacebookUserApi.Params) => Promise<void>
+  loadUserByToken: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
 }
 namespace LoadFacebookUserApi {
   export type Params = {
     token: string
   }
+  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
-  async loadUserByToken (params: LoadFacebookUserApi.Params): Promise<void> {
+  result = undefined
+  async loadUserByToken (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -33,5 +38,14 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'some_token' })
 
     expect(loadFacebookUserApi.token).toBe('some_token')
+  })
+
+  it('shoud return AuthenticationError when LoadFacebookUserApiSpy returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    const authResult = await sut.perform({ token: 'some_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
