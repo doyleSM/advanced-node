03e5263863e0d461fd3225761fce67b24186a11d
Gabriel Doyle <gdoyle.dev@gmail.com>
2021-07-18 22:51:39 -0300

    simplify code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -4,23 +4,27 @@ import { FacebookAuthenticationService } from '@/data/services'
 import { mock, MockProxy } from 'jest-mock-extended'
 import { LoadFacebookUserApi } from '../interfaces/apis'
 
+const token = 'any_token'
+
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
   let sut: FacebookAuthenticationService
+
   beforeEach(() => {
     loadFacebookUserApi = mock()
     sut = new FacebookAuthenticationService(loadFacebookUserApi)
   })
-  it('shoud call LoadFacebookUserApiSpy with correct params', async () => {
-    await sut.perform({ token: 'some_token' })
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'some_token' })
+  it('shoud call LoadFacebookUserApiSpy with correct params', async () => {
+    await sut.perform({ token })
+
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('shoud return AuthenticationError when LoadFacebookUserApiSpy returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValue(undefined)
-    const authResult = await sut.perform({ token: 'some_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
