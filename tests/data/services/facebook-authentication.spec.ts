0cc97e15b39bc014c5d4f1fd665948a3cbc2e2df
Gabriel Doyle <gdoyle.dev@gmail.com>
2021-07-18 22:35:11 -0300

    change mock strategy to use jest-mock-extended

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,11 +1,12 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthenticationService } from '@/data/services'
 
+import { mock } from 'jest-mock-extended'
+import { LoadFacebookUserApi } from '../interfaces/apis'
+
 describe('FacebookAuthenticationService', () => {
   it('shoud call LoadFacebookUserApiSpy with correct params', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     await sut.perform({ token: 'some_token' })
 
@@ -14,9 +15,8 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('shoud return AuthenticationError when LoadFacebookUserApiSpy returns undefined', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
+
     loadFacebookUserApi.loadUser.mockResolvedValue(undefined)
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     const authResult = await sut.perform({ token: 'some_token' })
