e40198fc0fce74467634b47fd07c8e13b8c7208c
Gabriel Doyle <gdoyle.dev@gmail.com>
2021-09-20 15:09:06 -0300

    ensure axios HttpClient thorws if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -56,5 +56,15 @@ describe('AxiosHttpClient', () => {
 
       expect(result).toEqual('any_data')
     })
+
+    it('should retrhow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+      const promise = sut.get({
+        url,
+        params
+      })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
