316d1103430e0152eede2361bbb88c8e679f6d47
Gabriel Doyle <gdoyle.dev@gmail.com>
2021-09-20 15:56:21 -0300

    ensure JwtTokenGenerator calls jwt with correct params

---
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -0,0 +1,29 @@
+import { TokenGenerator } from '@/data/interfaces/crypto'
+
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken')
+
+export class JwtTokenGenerator {
+  constructor (private readonly secret: string) {}
+  async generateToken (params: TokenGenerator.Params): Promise<void> {
+    const expirationInSeconds = params.expirationInMs / 1000
+
+    jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
+  }
+}
+
+describe('JwtTokenGenerator', () => {
+  it('Should call sign with correct params', async () => {
+    const sut = new JwtTokenGenerator('any_secret')
+
+    const fakeJwt = jwt as jest.Mocked<typeof jwt>
+
+    await sut.generateToken({
+      key: 'any_key',
+      expirationInMs: 1000
+    })
+
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+  })
+})
